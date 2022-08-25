b34d40b9df1d8f6dfcda3f993260fcb3b4062888
gabefando <gabe.fonseca.04@gmail.com>
2022-08-24 22:18:34 -0400

    got it to show and add notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const notes = fs.readFileSync('./db/db.json')
+var notes = require('./db/db.json')
 
 // from miniproject
 app.use(express.json());
@@ -15,6 +15,19 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    let notes2 = {
+        title: req.body.title,
+        text: req.body.text,
+    }
+
+    let newnote2 = JSON.stringify(notes);
+    
+    notes.push(notes2)
+
+    fs.writeFileSync(__dirname+"/db/db.json", newnote2);
 })
 
 app.get('/', (req, res) => {
