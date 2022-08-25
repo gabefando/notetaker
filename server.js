70553ae701ed536118b8123ca003ce9e47dc7752
gabefando <gabe.fonseca.04@gmail.com>
2022-08-24 21:52:41 -0400

    trying to get notes already in db to display

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const notes = fs.readFileSync('./db/db.json')
+
+// from miniproject
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+
 
 app.listen(3002)
